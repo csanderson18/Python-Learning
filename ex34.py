bbe82bd07641f3ddb68a562cc94afb175afb1103
Christian <csanderson18@bsmschool.org>
2017-10-10 12:34:37 -0500

    ye ye these be excercises

---
+++ ex34.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+# the animal at 1 is the 2nd animal and is a python
+#the third animal is at 2 and is a peacock
+#the first animal is at 0 and is a bear
+#the animal at 3 is the fourth animal and is a kangaroo
+#the fifth animal is at 4 and is a whale
+#the animal at 2 is the third animal and is a peacock
+#the sixth animal is at 5 and is a platypus
+#the animal iat 4 is the 5th animal and is a whale
